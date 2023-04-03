289b6db4a7f901dbc2cdcc890b5d23ff724653e3
Alexey Frolov <63521003+catpital@users.noreply.github.com>
2023-04-04 00:21:17 +0300

    v3_l

--- _main.cpp
+++ _main.cpp
@@ -8,6 +8,30 @@
 #include <fstream> 	// Для типа ifstream 
 #include <iostream>	// Для cout
 using namespace std;
+#include <iostream>
+using namespace std;
+
+// #if defined(_WIN32) 
+// #define PLATFORM_NAME "windows 32-bit" // Windows 32-bit
+// #elif defined(_WIN64)
+// #define PLATFORM_NAME "windows 64-bit" // Windows 64-bit
+// #elif defined(__ANDROID__)
+// #define PLATFORM_NAME "android"   // Android 
+// #elif defined(__linux__)
+// #define PLATFORM_NAME "linux"     // Debian, Ubuntu, Gentoo, Fedora, openSUSE, RedHat, Centos и другие
+// #elif TARGET_OS_IPHONE == 1
+// #define PLATFORM_NAME "ios"       // Apple iOS
+// #elif TARGET_OS_MAC == 1
+// #define PLATFORM_NAME "osx"       // Apple OSX
+// #else
+// #define PLATFORM_NAME  "OS not detected" // ОС не распознана
+// #endif
+
+// int main() {
+//     // Вывод значения макроса PLATFORM_NAME
+//     cout << PLATFORM_NAME << endl;
+//     return 0;
+// }
 
 //class User {
 //    std::string _name;
--- main.cpp
+++ main.cpp
@@ -87,41 +87,111 @@ void writemessagefile(Message _message) {
 
 void readuserfile() {
    
-    int fr = -1;
-    ifstream file("fileuser.txt");
-    if (fr == file.is_open())
-        //"fileuser.txt", O_RDWR | O_CREAT, 0777))
-    {
-        cout << "файл не существует" << endl;
-    }
+    fstream user_file = fstream("fileuser.txt", ios::in | ios::out);
+		if (!mess_file)
+		// 	// Для создания файла используем параметр ios::trunc
+		{ 	cout << "файл не существует" << endl;
+            cout << "создаем файл" << endl;
+            user_file = fstream("fileuser.txt", ios::in | ios::out | ios::trunc);
+            if Platform="linux"
+           { int stat = _chmod(userpath, S_IRUSR | S_IWUSR | S_IXUSR);
+           }
+       cout<<"создан пустой файл"<<endl;
+            return;}
+    
+   
     else {
-        int stat=_chmod(userpath, S_IRUSR | S_IWUSR | S_IXUSR);
-    }
+       int i = 1;
+       user_file.seekg(0, ios_base::beg);
+       while(!user_file.eof())
+       {
+			// 	 Считываем данные из файла
+				user_file >> user;;
+		// 		//user_file >> obj2;
+		cout <<"строчка "<< i << user << endl;
+		
+		i++;
+       }
+       user_file.close();
+           }
 }
 void readmessagefile() {
-    int fr = -1;
-    ifstream file("filemessage.txt");
-    if (fr == file.is_open())
-        //"fileuser.txt", O_RDWR | O_CREAT, 0777))
-    {
-        cout << "файл не существует" << endl;
-    }
+    // int fr = -1;
+    // ifstream file("filemessage.txt");
+    // if (fr == file.is_open())
+    //     //"fileuser.txt", O_RDWR | O_CREAT, 0777))
+    fstream mess_file = fstream("filemessage.txt", ios::in | ios::out);
+		if (!mess_file)
+		// 	// Для создания файла используем параметр ios::trunc
+		{ 	cout << "файл не существует" << endl;
+            cout << "создаем файл" << endl;
+            mess_file = fstream("filemessage.txt", ios::in | ios::out | ios::trunc);
+            if Platform="linux"
+           { int stat = _chmod(messagepath, S_IRUSR | S_IWUSR | S_IXUSR);
+           }
+       cout<<"создан пустой файл"<<endl;
+            return;}
+    
+   
     else {
-        int stat = _chmod(messagepath, S_IRUSR | S_IWUSR | S_IXUSR);
-    }
+       int i = 1;
+       mess_file.seekg(0, ios_base::beg);
+       while(!mess_file.eof())
+       {
+			// 	 Считываем данные из файла
+				mess_file >> message;;
+		// 		//user_file >> obj2;
+		cout <<"строчка "<< i << message << endl;
+		
+		i++;
+       }
+       mess_file.close();
+           }
 }
-
-
+// fstream user_file = fstream("users.txt", ios::in | ios::out);
+		// if (!user_file)
+		// 	// Для создания файла используем параметр ios::trunc
+		// 	user_file = fstream("users.txt", ios::in | ios::out | ios::trunc);
+	
+		// if (user_file) {
+		// 	User obj1("Alex", "qwerty", "12345");
+		// 		// Запишем данные  в файл
+		// 	user_file << obj1 << endl;
+		// 	User obj2("Alexander", "next", "456678");
+		// 	user_file << obj2 << endl;
+		// 	// Чтобы считать данные из файла, надо установить позицию для чтения в начало файла
+		// 	user_file.seekg(0, ios_base::beg);
+		// 	// Считываем данные из файла
+		// 	//int i = 1;
+		// 	while (!user_file.eof()) {
+		// 		user_file >> obj1;
+		// 		//user_file >> obj2;
+		// 		//cout <<" 1 obj" << obj1 << endl;
+		// 		cout << obj1 << endl;
+		// 		//i++;
+		// 	}
+		// }
+		// else
+		// {
+		// 	cout << "Could not open file users.txt !" << '\n';
+		// 	return 0;
+		// }
+		// user_file.close();
+if defined(__linux__)
+#define PLATFORM_NAME "linux"
 int main()
 {
+    string Platform=PLATFORM_NAME;
+
     readuserfile();
     User user("Alex", "alex", "12345");
     writeuserfile(user);
-    readuserfile();
-    readmessagefile();
-    Message message("Vsem privet", "Alex", "ALL");
-    writemessagefile(message);
-    readmessagefile();
+   readuserfile();
+   //readmessagefile();
+    //Message message("Vsem privet", "Alex", "ALL");
+    //writemessagefile(message);
+    //readmessagefile();
+    
 
 	return 0;
 }
