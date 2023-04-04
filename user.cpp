be61d19c7c78252918ee94b33ca8a3c8eb10a329
Alexey Frolov <63521003+catpital@users.noreply.github.com>
2023-04-05 00:34:31 +0300

    v4

--- user.cpp
+++ user.cpp
@@ -7,11 +7,16 @@
 #include <io.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <vector>
+//#include <sstream>
+//#include <filesystem>
+using namespace std;
+//using namespace std::filesystem;
 //int S_IRUSR ;
 //int S_IWUSR ;
 //int S_IXUSR ;
 #define userpath "~\tmp\fileuser.txt"
-using namespace std;
+
 
 class User {
 private:
@@ -23,6 +28,7 @@ public:
     string login;
     string pass;
     User(string _name, string _login, string _pass) : name(_name), login(_login), pass(_pass) {}
+    User();
     ~User();
     friend fstream& operator >>(fstream& is, User& obj);
     friend ostream& operator <<(ostream& os, const User& obj);
@@ -49,8 +55,8 @@ void writeuserfile(User _user) {
 }
 
 void readuserfile() {
-
-    fstream user_file = fstream("fileuser.txt", ios::in | ios::out);
+    //path userpath(L"~\tmp\fileuser.txt");
+    fstream user_file = fstream(userpath, ios::in | ios::out);
     if (!user_file)
         // 	// Для создания файла используем параметр ios::trunc
     {
@@ -68,14 +74,16 @@ void readuserfile() {
 
     else {
         int i = 1;
-        
         user_file.seekg(0, ios_base::beg);
+        vector<User> user_t;
+        User user;
         while (!user_file.eof())
         {
             // 	 Считываем данные из файла
-            user_file >> user;;
+            user_file >> user;
+            user_t.push_back(user);
             // 		//user_file >> obj2;
-            cout << "строчка " << i << user << endl;
+            cout << "строчка " << i << user_t.back() << endl;
              i++;
         }
         user_file.close();
